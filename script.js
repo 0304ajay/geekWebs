93b98acaf7e4d06db2b29668b7c73a0a260afb1d
Kodidela Ajay Kumar Reddy <125742641+0304ajay@users.noreply.github.com>
2023-12-29 17:00:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function mylogin()
+{
+  var a=prompt("Enter Username");
+  var b=prompt("Enter Password");
+  
+    alert("Login Successful");
+
+}
+
+function signup()
+{
+  var a=prompt("Enter first name:");
+  var b=prompt("Enter last name:");
+  var c=prompt("Enter password:");
+  alert("Signup Successful");
+  }
+
+function newsletter()
+{
+  var a=prompt("Enter Email Address:");
+  alert("You will recieve Updates Via Email");
+}
+function handle()
+{
+  alert('Your web page is being redirected');
+}
+// #js {
+//   display: inline;
+//   text-align: center;
+//   margin-top: 20px;
+//   padding: 40px;
+//   background-color: white;
+//   height: auto;
+//   width: auto;
+// }
